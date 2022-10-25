7f520ef6b5e6d18cea24eed6166e7785e8e5cc47
Seungji <96293047+tuz22@users.noreply.github.com>
2022-10-25 23:06:43 +0900

    Component

--- src/App.js
+++ src/App.js
@@ -7,30 +7,6 @@ import { useState } from 'react';
 
 function App() {
 
-  /* 
-    state 사용방법
-    1. import {useState}
-    2. useState(보관할 자료)
-    3. let[변수1, 변수2]
-        - 변수1: state에 보관했던 자료
-        - 변수2: state 변경을 도와주는 함수
-    
-    state 사용 이유
-    - 변수의 경우 변수 값이 변경되면 직접 변경시켜줘야함
-    - state의 경우 값이 변경되면 state를 사용하던 HTML이 자동으로 재렌더링됨
-
-    state 변경시
-    - 등호로 변경 x
-
-    state 변경함수 특징
-    - 기존state == 신규state의 경우 변경 안해줌
-
-    array/object 특징
-    - array/object를 저장한 변수에 화살표만 저장
-      (= 여기에 이 값이 있다~ 정도)
-
-  */
-
   let post = '서브웨이 조합 추천';
   let [title, titleChange] = useState(['파스타가 맛있는 가게', '노브랜드 신상 버거: 맥앤치즈버거', '뫄뫄 타코야끼 후기']);
   let [like, likeCount] = useState(0);
@@ -42,11 +18,8 @@ function App() {
       </div>
 
       <button onClick={ () => {
-        // title = [...title];
-        // title[0] = '디저트가 맛있는 카페';
-        // titleChange(title);
-        let copy = [...title]; // ... 사용이유
-        copy[0] = '디저트가 맛있는 카페';  // copy란 state가 변경 되는 게 아니라, copy[0]의 값이 '디저트~'로 변경되었다는 것이기 때문 => title == copy로 간주되어 변경안해줌
+        let copy = [...title];
+        copy[0] = '디저트가 맛있는 카페';
         titleChange(copy);
       }}>🧁CLICK!
       </button>
@@ -63,8 +36,68 @@ function App() {
         <h4>{ title[2] }</h4>
         <h4>2022-10-24</h4>
       </div>
-    </div> /* return() 안에는 병렬로 태그 2개이상 x */
+
+      <Modal></Modal> {/* <Modal />과 같음 */}
+      
+
+    </div>
   );
 }
+
+/* 
+  컴포넌트 만드는 방법
+  1. function 만들기
+  2. return() 안에 HTML 담기
+  3. <함수명></함수명> 쓰기
+*/
+
+function Modal(){ // Modal - 컴포넌트
+  return(
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
+
+// <div>를 굳이 병렬로 사용해야한다면? - 밖에 <div>로 감싸주기
+// 의미 없는 <div>가 싫다면? - <Fragment> 사용하기 
+
+function Modal2(){
+  return(
+    <> {/* <- Fragment 단축 */}
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
+  )
+}
+
+// 컴포넌트를 만드는 방법2
+// const Modal = () => {
+//   return (
+//     <div></div>
+//   )
+// }
+
+/* 
+  컴포넌트 사용하기 좋을 때
+  1. 반복적인 HTML을 축약할 때
+  2. 페이지 전환을 할때 사용하는 큰 페이지들
+  3. 자주 변경되는 것들
+*/
+
+/* 
+  컴포넌트 단점
+  1. state를 가져다 쓸 때 문제가 생김 ex) not defined... <- 변수의 범위가 함수라서 가져다 쓸 수 없음..
+*/
 
 export default App;
