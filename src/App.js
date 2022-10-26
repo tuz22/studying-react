4a657164781732e9e12cfe1ef36701d6b9b41481
Seungji <96293047+tuz22@users.noreply.github.com>
2022-10-27 03:53:10 +0900

    props

--- src/App.js
+++ src/App.js
@@ -6,22 +6,16 @@ import { useState } from 'react';
 
 function App() {
 
-  let [title, titleChange] = useState(['파스타가 맛있는 가게', '노브랜드 신상 버거: 맥앤치즈버거', '뫄뫄 타코야끼 후기']);
+  let [title] = useState(['파스타가 맛있는 가게', '노브랜드 신상 버거: 맥앤치즈버거', '뫄뫄 타코야끼 후기']);
   let [like, likeCount] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [detailTitle, setTitle] = useState(0);
 
   return (
     <div className='App'>
       <div className='black-nav'>
         <h4>React-Study</h4>
       </div>
-
-      <button onClick={ () => {
-        let copy = [...title];
-        copy[0] = '디저트가 맛있는 카페';
-        titleChange(copy);
-      }}>🧁CLICK!
-      </button>
 
       {/* <div className='list'>
         <h4>{ title[0] } <span onClick={ () => {likeCount(like+1)} }>❤</span> {like} </h4>
@@ -41,7 +35,7 @@ function App() {
         title.map(function(data, i){
           return (
             <div className='list' key={i}>
-              <h4 onClick={ () => { modal == false ? setModal(true) : setModal(false) }}>{ title[i] }
+              <h4 onClick={ () => { modal == false ? setModal(true)||setTitle(i) : setModal(false) }}>{ title[i] }
                 <span onClick={ () => {
                   let copy = [...like];
                   copy[i] = copy[i] + 1;
@@ -56,7 +50,7 @@ function App() {
       }
       
       {
-        modal == true ? <Modal title={title} titleChange={titleChange} color={'skyblue'}/> : null /* color='skyblue'도 가능 */
+        modal == true ? <Modal title={title} detailTitle={detailTitle}/> : null
       }
     </div>
   );
@@ -69,16 +63,20 @@ function App() {
   ** 컴포넌트1 -> 컴포넌트2도 x
 */
 function Modal(props){
+  // let [detailTitle, setTitle] = useState(0);
+  /* state를 App말고 여기에 만들면 부모 -> 자식state 전송 필요x */
+  // 근데 state가 Modal, App에서 둘다 필요하면 가장 상위 컴포넌트(App)에 만들어야함
+
   return(
-    <div className='modal' style={ {background:props.color}}> {/* 응용. 여러색깔 모달창은? */}
-      <h4>{props.title[0]}</h4>
+    <div className='modal'>
+      <h4>{props.title[props.detailTitle]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
-      <button onClick={ () => { 
+      {/* <button onClick={ () => { 
         let copy = [...props.title];
         copy[0] = '디저트가 맛있는 카페';
         props.titleChange(copy);
-        }}>글 수정</button>
+        }}>글 수정</button> */}
     </div>
   )
 }
